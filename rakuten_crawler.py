3f9487bd2f95d750ae3a7f5a1c710d401e272317
Masaru Gotou <masaru.gotou@gmail.com>
2020-04-13 02:49:48 +0900

    set rakuten timeout longer. update driver helper usage.

--- rakuten_crawler.py
+++ rakuten_crawler.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 import sys
 from argparse import ArgumentParser
 from selenium_helper import SeleniumHelper
+import selenium_helper as helper
 
 OUTDIR_SS='./file/ss/'
 OUTDIR='./file/'
@@ -45,89 +46,98 @@ user_id = args.user_id
 passwd = args.passwd
 outdir = args.outdir
 
-# ブラウザを起動
-options = Options()
-options.add_argument('--headless')
-driver = webdriver.Chrome(chrome_options=options)
-driver.set_page_load_timeout(10)
+helper.outdir_ss = OUTDIR_SS
+helper.is_save_html_with_ss = True;
+log.info("outdir_ss={}".format(helper.outdir_ss))
 
-# windowサイズを変更
-win_size = driver.get_window_size()
-driver.set_window_size(win_size['width']+200,win_size['height']+400)
+try:
+    # ブラウザを起動
+    driver = helper.start_browser()
+#    options = Options()
+#    options.add_argument('--headless')
+#    driver = webdriver.Chrome(chrome_options=options)
+    driver.set_page_load_timeout(30)
 
-helper = SeleniumHelper(driver, OUTDIR_SS)
+    # windowサイズを変更
+#    win_size = driver.get_window_size()
+#    driver.set_window_size(win_size['width']+200,win_size['height']+400)
 
-# 楽天ログイン画面
-log.info("getting login page")
-driver.get('https://www.rakuten-card.co.jp/e-navi/')
-helper.ss(name='login')
+#    helper = SeleniumHelper(driver, OUTDIR_SS)
 
-# ログイン
-log.info("logging in to the site...")
-e_user = driver.find_element_by_id('u')
-e_password = driver.find_element_by_id('p')
-e_user.send_keys(user_id)
-e_password.send_keys(passwd)
-e_button = driver.find_element_by_id('loginButton')
-helper.ss(name='before-login')
-e_button.click()
-helper.ss(name='top')
+    # 楽天ログイン画面
+    log.info("getting login page")
+    driver.get('https://www.rakuten-card.co.jp/e-navi/')
+    helper.ss(name='login')
 
-# ご利用明細へのリンクを探す
-log.info("navigate to bill-detail..")
-e_link = driver.find_element_by_xpath('//ul[@class="rce-u-list-plain"]//a[text()="ご利用明細"]')
-log.debug('link for detail : tag={} href={} visible={}'.format(e_link.tag_name, e_link.get_attribute('href'),e_link.is_displayed()))
-#helper.ss(name='detail_link')
-# ご利用明細
-e_link.click()
-helper.ss(name='detail')
+    # ログイン
+    log.info("logging in to the site...")
+    e_user = driver.find_element_by_id('u')
+    e_password = driver.find_element_by_id('p')
+    e_user.send_keys(user_id)
+    e_password.send_keys(passwd)
+    e_button = driver.find_element_by_id('loginButton')
+    helper.ss(name='before-login')
+    e_button.click()
+    helper.ss(name='top')
 
-# 確定かどうか判定
-#e_bill = driver.find_element_by_xpath('//div[@class="stmt-bill-info-amount__main"]/span[contains(@class,"mark")]')
-e_bill = driver.find_element_by_xpath('//div[@class="stmt-bill-info-amount__main"]/*[contains(@class,"mark")]')
-log.debug('bill-text:{}'.format(e_bill.text))
+    # ご利用明細へのリンクを探す
+    log.info("navigate to bill-detail..")
+    e_link = driver.find_element_by_xpath('//ul[@class="rce-u-list-plain"]//a[text()="ご利用明細"]')
+    log.debug('link for detail : tag={} href={} visible={}'.format(e_link.tag_name, e_link.get_attribute('href'),e_link.is_displayed()))
+    #helper.ss(name='detail_link')
+    # ご利用明細
+    e_link.click()
+    helper.ss(name='detail')
 
-# 未確定なら支払い予定額を保存
-if '未確定' in e_bill.text:
-    write_future_expence(driver, outdir)
-    log.info("end")
-    exit()
+    # 確定かどうか判定
+    #e_bill = driver.find_element_by_xpath('//div[@class="stmt-bill-info-amount__main"]/span[contains(@class,"mark")]')
+    e_bill = driver.find_element_by_xpath('//div[@class="stmt-bill-info-amount__main"]/*[contains(@class,"mark")]')
+    log.debug('bill-text:{}'.format(e_bill.text))
 
-# CSVダウンロードのリンクを探す
-log.info("downloading csv..")
-e_csv_link = driver.find_element_by_xpath('//a[contains(.,"CSV")]')
-log.debug('link for csv : tag={} href={}'.format(e_csv_link.tag_name, e_csv_link.get_attribute('href')))
+    # 未確定なら支払い予定額を保存
+    if '未確定' in e_bill.text:
+        write_future_expence(driver, outdir)
+        log.info("end")
+        exit()
 
-# ダウンロード可能にする
-driver.command_executor._commands["send_command"] = (
-    "POST",
-    '/session/$sessionId/chromium/send_command'
-)
-params = {
-    'cmd': 'Page.setDownloadBehavior',
-    'params': {
-        'behavior': 'allow',
-        'downloadPath': outdir
+    # CSVダウンロードのリンクを探す
+    log.info("downloading csv..")
+    e_csv_link = driver.find_element_by_xpath('//a[contains(.,"CSV")]')
+    log.debug('link for csv : tag={} href={}'.format(e_csv_link.tag_name, e_csv_link.get_attribute('href')))
+
+    # ダウンロード可能にする
+    driver.command_executor._commands["send_command"] = (
+        "POST",
+        '/session/$sessionId/chromium/send_command'
+    )
+    params = {
+        'cmd': 'Page.setDownloadBehavior',
+        'params': {
+            'behavior': 'allow',
+            'downloadPath': outdir
+        }
     }
-}
-driver.execute("send_command", params=params)
+    driver.execute("send_command", params=params)
 
-# download csv
-driver.get(e_csv_link.get_attribute('href'))
-helper.ss(name='csv_downloaded')
+    # download csv
+    driver.get(e_csv_link.get_attribute('href'))
+    helper.ss(name='csv_downloaded')
 
-# 次月が押せたら次月を押す
-log.info("navigate to next month ..")
-e_next_link = driver.find_element_by_xpath('//a[text()="次月"]')
-log.debug('link for next month : tag={} href={} visible={}'.format(e_next_link.tag_name, e_next_link.get_attribute('href'),e_next_link.is_displayed()))
-e_next_link.click()
-helper.ss(name='next_month')
+    # 次月が押せたら次月を押す
+    log.info("navigate to next month ..")
+    e_next_link = driver.find_element_by_xpath('//a[text()="次月"]')
+    log.debug('link for next month : tag={} href={} visible={}'.format(e_next_link.tag_name, e_next_link.get_attribute('href'),e_next_link.is_displayed()))
+    e_next_link.click()
+    helper.ss(name='next_month')
 
-# 支払い予定金額出力
-write_future_expence(driver, outdir)
+    # 支払い予定金額出力
+    write_future_expence(driver, outdir)
 
-driver.close()
-driver.quit()
-log.info("end")
+finally:
+    if ( driver is not None ):
+        driver.quit()
+        log.info("WebDriver Quit")
+    log.info("end")
+
 exit()
 
