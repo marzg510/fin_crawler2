7941dad98f98aaf56acc4df1e702b21479abbe9a
Masaru Gotou <masaru.gotou@gmail.com>
2021-09-21 21:43:46 +0900

    modify smbc crawler

--- smbc_crawler.py
+++ smbc_crawler.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
-import logging,logging.handlers
+import logging
+import logging.handlers
 import sys
 import os
 from argparse import ArgumentParser
@@ -8,15 +9,15 @@ import selenium_helper as helper
 import time
 import selenium.common.exceptions
 
-OUTDIR_SS='./file/ss_smbc'
-OUTDIR='./file/'
-LOGDIR='./log/'
+OUTDIR_SS = './file/ss_smbc'
+OUTDIR = './file/'
+LOGDIR = './log/'
 
 # ログ設定
 ap_name = os.path.splitext(os.path.basename(__file__))[0]
 log = logging.getLogger(ap_name)
 log.setLevel(logging.DEBUG)
-h = logging.handlers.TimedRotatingFileHandler('{}/{}.log'.format(LOGDIR,ap_name),'D',2,13)
+h = logging.handlers.TimedRotatingFileHandler('{}/{}.log'.format(LOGDIR, ap_name), 'D', 2, 13)
 h.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s"))
 log.addHandler(h)
 
@@ -37,12 +38,12 @@ passwd = args.passwd
 account = args.account
 outdir = args.outdir
 
-log.info("user_id={} {}".format(user1,user2))
+log.info("user_id={} {}".format(user1, user2))
 log.info("account={}".format(account))
 log.info("outdir={}".format(outdir))
 
-outfile = os.path.join(outdir, 'smbc_{}_{}.csv'.format(account, time.strftime('%Y%m%d',time.localtime())))
-log.info ("outfile=%s" % outfile)
+outfile = os.path.join(outdir, 'smbc_{}_{}.csv'.format(account, time.strftime('%Y%m%d', time.localtime())))
+log.info("outfile=%s" % outfile)
 
 # 処理開始
 try:
@@ -59,7 +60,7 @@ try:
     time.sleep(1)
     helper.ss(name='smbc_login')
 
-    ############# system maintainance check
+    # ############# system maintainance check
     try:
         e_sorry = driver.find_element_by_css_selector('div#sorry')
         log.error("システムメンテナンス中")
@@ -70,30 +71,38 @@ try:
 
     # ログイン
     log.info("logging in to the site...")
-    e_user1 = driver.find_element_by_id('USRID1')
-    e_user2 = driver.find_element_by_id('USRID2')
-    e_password = driver.find_element_by_id('PASSWORD')
-    e_type = driver.find_element_by_id('LOGIN_TYPE')
+
+    e_tab = driver.find_element_by_id('tab-switchB02')
+    e_tab.click()
+    time.sleep(1)
+    helper.ss(name='smbc_login_tab_clicked')
+
+    e_user1 = driver.find_element_by_name('userId1')
+    e_user2 = driver.find_element_by_name('userId2')
+    e_password = driver.find_element_by_id('inputpassword')
+    # e_type = driver.find_element_by_id('LOGIN_TYPE')
     e_user1.send_keys(user1)
     e_user2.send_keys(user2)
     e_password.send_keys(passwd)
-    driver.execute_script('var e = arguments[0];var v = arguments[1];e.value = v;',
-                           e_type, '0')
-    e_submit = driver.find_element_by_xpath('//input[@type="submit" and @name="bLogon.y"]')
+    # driver.execute_script('var e = arguments[0];var v = arguments[1];e.value = v;',
+                        #    e_type, '0')
     helper.ss(name='before-login')
+    e_submit = driver.find_element_by_xpath("//div[@class='btn-wrap']/a[.='ログイン']")
+    # e_submit = driver.find_element_by_xpath('//input[@type="submit" and @name="bLogon.y"]')
     e_submit.click()
+    time.sleep(3)
     helper.ss(name='after-login')
 
-    ############# login check
-    e_errs = driver.find_elements_by_css_selector('div.CMNerrorMessage')
+    # ############ login check
+    e_errs = driver.find_elements_by_xpath("//dt[@class='title' and contains(text(),'エラーコード')]")
     if len(e_errs) > 0:
         log.error("login failure")
-        for err in errs:
+        for err in e_errs:
             log.error(err)
-        sys.exit(9)
+        raise Exception('login failure')
 
-    ############# confirm message
-    log.info ("confirming message")
+    # ############ confirm message
+    log.info("confirming message")
     try:
         e_next = driver.find_element_by_xpath('//input[@type="button" and @name="imgNext.y"]')
         e_next.click()
@@ -101,34 +110,33 @@ try:
         log.exception('exception occurred,but coutinueing process.')
     helper.ss(name='smbc-top-after-confirm')
 
-    ############# Navigate to Detail page
-    log.info ("Navigate to Detail page")
-    e_link = driver.find_element_by_xpath('//a[contains(text(),"'+account+'")]')
-    log.debug('link for detail : tag={} href={} visible={}'.format(e_link.tag_name, e_link.get_attribute('href'),e_link.is_displayed()))
+    # ############ Navigate to Detail page
+    log.info("Navigate to Detail page")
+    e_link = driver.find_element_by_xpath("//a[contains(.,'{}')]".format(account))
+    log.debug('link for detail : tag={} href={} visible={}'.format(e_link.tag_name, e_link.get_attribute('href'), e_link.is_displayed()))
     e_link.click()
+    time.sleep(1)
     helper.ss(name='detail')
 
-    ############# Download CSV file
-    log.info ("Downloading")
-    e_csv = driver.find_element_by_xpath('//a[@id="DownloadCSV"]')
+    # ############ Download CSV file
+    log.info("Downloading")
+    e_csv = driver.find_element_by_xpath("//a[contains(.,'明細をCSVダウンロード')]")
     log.debug('link for csv : tag={} href={} visible={}'.format(e_csv.tag_name, e_csv.get_attribute('href'),e_csv.is_displayed()))
     e_csv.click()
-#    with open(outfile, 'wb') as out:
-#        out.write(br.response.content)
-    log.info ("finish file writing to {}".format(outdir))
+    log.info("finish file writing to {}".format(outdir))
     helper.ss(name='after-csv-downloaded')
 
     # rename
     time.sleep(10)
-    os.rename(os.path.join(outdir,'meisai.csv'),outfile)
-    log.info ("rename file to {}".format(outfile))
+    os.rename(os.path.join(outdir, 'meisai.csv'), outfile)
+    log.info("rename file to {}".format(outfile))
 
 except Exception as e:
     log.exception('exception occurred.')
     print(e, file=sys.stderr)
 
 finally:
-    if ( driver is not None ):
+    if (driver is not None):
         driver.quit()
         log.info("WebDriver Quit")
     log.info("end")
