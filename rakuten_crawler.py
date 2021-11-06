128ee518ed41367aa08b7ba787b756392db5cc94
Masaru Gotou <masaru.gotou@gmail.com>
2021-11-06 21:30:25 +0900

    refactor rakuten

--- rakuten_crawler.py
+++ rakuten_crawler.py
@@ -1,26 +1,26 @@
 # coding: utf-8
 
-import logging,logging.handlers
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import sys,os
+import logging
+import logging.handlers
+import os
 from argparse import ArgumentParser
 import selenium_helper as helper
+from selenium.webdriver.common.by import By
 
-OUTDIR_SS='./file/ss/'
-OUTDIR='./file/'
-LOGDIR='./log/'
+OUTDIR_SS = './file/ss/'
+OUTDIR = './file/'
+LOGDIR = './log/'
 
 
 def write_future_expence(driver, outdir):
     global log
     log.info("write furure expence to %s" % outdir)
-    e_month = driver.find_element_by_xpath('//div[@id="js-payment-calendar"]')
+    e_month = driver.find_element(By.XPATH, '//div[@id="js-payment-calendar"]')
     log.debug('month : tag={} text={}'.format(e_month.tag_name, e_month.text))
-    e_bill = driver.find_element_by_xpath('//div[@class="stmt-about-payment__money__main"]')
-    e_mark = e_bill.find_element_by_xpath('mark')
+    e_bill = driver.find_element(By.XPATH, '//div[@class="stmt-about-payment__money__main"]')
+    e_mark = e_bill.find_element(By.XPATH, 'mark')
     log.debug('mark : tag={} text={}'.format(e_mark.tag_name, e_mark.text))
-    e_amount = e_bill.find_element_by_xpath('div[@class="stmt-about-payment__money__main__num"]')
+    e_amount = e_bill.find_element(By.XPATH, 'div[@class="stmt-about-payment__money__main__num"]')
     log.debug('amount : tag={} text={}'.format(e_amount.tag_name, e_amount.text))
     with open(os.path.join(outdir, '%s_%s.txt' % (e_month.text, e_mark.text)), 'wt') as fout:
         fout.write(e_amount.text)
@@ -30,7 +30,7 @@ def write_future_expence(driver, outdir):
 ap_name = os.path.splitext(os.path.basename(__file__))[0]
 log = logging.getLogger(ap_name)
 log.setLevel(logging.DEBUG)
-h = logging.handlers.TimedRotatingFileHandler('{}/{}.log'.format(LOGDIR,ap_name),'D',2,13)
+h = logging.handlers.TimedRotatingFileHandler('{}/{}.log'.format(LOGDIR, ap_name), 'D', 2, 13)
 h.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s"))
 log.addHandler(h)
 
@@ -48,7 +48,7 @@ passwd = args.passwd
 outdir = args.outdir
 
 helper.outdir_ss = OUTDIR_SS
-helper.is_save_html_with_ss = True;
+helper.is_save_html_with_ss = True
 log.info("outdir_ss={}".format(helper.outdir_ss))
 
 # 処理開始
@@ -65,27 +65,25 @@ try:
 
     # ログイン
     log.info("logging in to the site...")
-    e_user = driver.find_element_by_id('u')
-    e_password = driver.find_element_by_id('p')
+    e_user = driver.find_element(By.ID, 'u')
+    e_password = driver.find_element(By.ID, 'p')
     e_user.send_keys(user_id)
     e_password.send_keys(passwd)
-    e_button = driver.find_element_by_id('loginButton')
+    e_button = driver.find_element(By.ID, 'loginButton')
     helper.ss(name='before-login')
     e_button.click()
     helper.ss(name='top')
 
     # ご利用明細へのリンクを探す
     log.info("navigate to bill-detail..")
-    e_link = driver.find_element_by_xpath('//ul[@class="rce-u-list-plain"]//a[text()="ご利用明細"]')
-    log.debug('link for detail : tag={} href={} visible={}'.format(e_link.tag_name, e_link.get_attribute('href'),e_link.is_displayed()))
-    #helper.ss(name='detail_link')
+    e_link = driver.find_element(By.XPATH, '//ul[@class="rce-u-list-plain"]//a[text()="ご利用明細"]')
+    log.debug('link for detail : tag={} href={} visible={}'.format(e_link.tag_name, e_link.get_attribute('href'), e_link.is_displayed()))
     # ご利用明細
     e_link.click()
     helper.ss(name='detail')
 
     # 確定かどうか判定
-    #e_bill = driver.find_element_by_xpath('//div[@class="stmt-bill-info-amount__main"]/span[contains(@class,"mark")]')
-    e_bill = driver.find_element_by_xpath('//div[@class="stmt-about-payment__money__main"]')
+    e_bill = driver.find_element(By.XPATH, '//div[@class="stmt-about-payment__money__main"]')
     log.debug('bill-text:{}'.format(e_bill.text))
 
     # 未確定なら支払い予定額を保存
@@ -96,7 +94,7 @@ try:
 
     # CSVダウンロードのリンクを探す
     log.info("downloading csv..")
-    e_csv_link = driver.find_element_by_xpath('//a[contains(.,"CSV")]')
+    e_csv_link = driver.find_element(By.XPATH, '//a[contains(.,"CSV")]')
     log.debug('link for csv : tag={} href={}'.format(e_csv_link.tag_name, e_csv_link.get_attribute('href')))
 
     # download csv
@@ -105,8 +103,8 @@ try:
 
     # 次月が押せたら次月を押す
     log.info("navigate to next month ..")
-    e_next_link = driver.find_element_by_xpath('//a[text()="次月"]')
-    log.debug('link for next month : tag={} href={} visible={}'.format(e_next_link.tag_name, e_next_link.get_attribute('href'),e_next_link.is_displayed()))
+    e_next_link = driver.find_element(By.XPATH, '//a[text()="次月"]')
+    log.debug('link for next month : tag={} href={} visible={}'.format(e_next_link.tag_name, e_next_link.get_attribute('href'), e_next_link.is_displayed()))
     e_next_link.click()
     helper.ss(name='next_month')
 
@@ -114,10 +112,9 @@ try:
     write_future_expence(driver, outdir)
 
 finally:
-    if ( driver is not None ):
+    if driver is not None:
         driver.quit()
         log.info("WebDriver Quit")
     log.info("end")
 
 exit()
-
