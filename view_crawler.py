66af23583ff78dd69c46000ec7604c5f3e0ea9bd
Masaru Gotou <masaru.gotou@gmail.com>
2021-11-06 21:34:34 +0900

    refactor view card

--- view_crawler.py
+++ view_crawler.py
@@ -1,22 +1,24 @@
 # coding: utf-8
 
-import logging,logging.handlers
+import logging
+import logging.handlers
 import sys
 import os
 from argparse import ArgumentParser
 import selenium_helper as helper
 import time
 import selenium.common.exceptions
+from selenium.webdriver.common.by import By
 
-OUTDIR_SS='./file/ss_view'
-OUTDIR='./file/'
-LOGDIR='./log/'
+OUTDIR_SS = './file/ss_view'
+OUTDIR = './file/'
+LOGDIR = './log/'
 
 # ログ設定
 ap_name = os.path.splitext(os.path.basename(__file__))[0]
 log = logging.getLogger(ap_name)
 log.setLevel(logging.DEBUG)
-h = logging.handlers.TimedRotatingFileHandler('{}/{}.log'.format(LOGDIR,ap_name),'D',2,13)
+h = logging.handlers.TimedRotatingFileHandler('{}/{}.log'.format(LOGDIR, ap_name), 'D', 2, 13)
 h.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s"))
 log.addHandler(h)
 
@@ -49,12 +51,10 @@ try:
     log.info("getting VIEW's NET top page")
     driver.get('https://www.jreast.co.jp/card/servicelist/viewsnet/login.html')
     helper.ss(name='view_top')
-    #log.debug('cookies = {}'.format(driver.get_cookies()))
 
     # VIEW's NETのログイン画面
     log.info("getting VIEW's NET login page")
-    e_login_link = driver.find_element_by_xpath('//a[contains(.,"VIEW") and contains(.,"s NETのIDで") and contains(.,"ログイン")]')
-    #log.debug('login_link = tag={},href={}'.format(e_login_link.tag_name,e_login_link.get_attribute('href')))
+    e_login_link = driver.find_element(By.XPATH, '//a[contains(.,"VIEW") and contains(.,"s NETのIDで") and contains(.,"ログイン")]')
     e_login_link.click()
     login_url = driver.current_url
     log.info('login url={}'.format(login_url))
@@ -63,44 +63,44 @@ try:
 
     # ログイン
     log.info("logging in to the site...")
-    e_user = driver.find_element_by_id('id')
-    e_password = driver.find_element_by_id('pass')
+    e_user = driver.find_element(By.ID, 'id')
+    e_password = driver.find_element(By.ID, 'pass')
     e_user.send_keys(user_id)
     e_password.send_keys(passwd)
-    e_button = driver.find_element_by_xpath('//input[@type="image" and @alt="ログイン"]')
+    e_button = driver.find_element(By.XPATH, '//input[@type="image" and @alt="ログイン"]')
     helper.ss(name='before-login')
     e_button.click()
     helper.ss(name='view_top')
 
     # ご利用明細照会
     log.info("navigate to bill-detail..")
-    e_link = driver.find_element_by_xpath('//a[@alt="ご利用明細照会（お支払方法の変更）"]')
-    log.debug('link for detail : tag={} href={} visible={}'.format(e_link.tag_name, e_link.get_attribute('href'),e_link.is_displayed()))
+    e_link = driver.find_element(By.XPATH, '//a[@alt="ご利用明細照会（お支払方法の変更）"]')
+    log.debug('link for detail : tag={} href={} visible={}'.format(e_link.tag_name, e_link.get_attribute('href'), e_link.is_displayed()))
     e_link.click()
     helper.ss(name='detail')
 
     # 確定済みの最新明細
-    e_bill_link = driver.find_element_by_id('LnkClaimYm1')
-    log.debug('link for bill : tag={} href={} visible={}'.format(e_bill_link.tag_name, e_bill_link.get_attribute('href'),e_bill_link.is_displayed()))
+    e_bill_link = driver.find_element(By.ID, 'LnkClaimYm1')
+    log.debug('link for bill : tag={} href={} visible={}'.format(e_bill_link.tag_name, e_bill_link.get_attribute('href'), e_bill_link.is_displayed()))
     e_bill_link.click()
     helper.ss(name='bill')
 
     # CSVダウンロード
     log.info("downloading csv..")
     try:
-        e_csv_link = driver.find_element_by_id('BtnCsvDownloadTop')
+        e_csv_link = driver.find_element(By.ID, 'BtnCsvDownloadTop')
     except selenium.common.exceptions.NoSuchElementException:
-        e_mesg = driver.find_element_by_id('DivErrorMessage')
+        e_mesg = driver.find_element(By.ID, 'DivErrorMessage')
         log.info(e_mesg.text.strip())
         log.info('no bill in this month')
     else:
-        log.debug('link for csv : tag={} href={} visible={}'.format(e_csv_link.tag_name, e_csv_link.get_attribute('alt'),e_csv_link.is_displayed()))
+        log.debug('link for csv : tag={} href={} visible={}'.format(e_csv_link.tag_name, e_csv_link.get_attribute('alt'), e_csv_link.is_displayed()))
         e_csv_link.click()
         helper.ss(name='csv_downloaded')
         # 請求予定の明細
         log.info("navigate to future bill ..")
-        e_next_link = driver.find_element_by_id('vucV0300MonthList_LnkYotei')
-        log.debug('link for next month : tag={} href={} visible={}'.format(e_next_link.tag_name, e_next_link.get_attribute('href'),e_next_link.is_displayed()))
+        e_next_link = driver.find_element(By.ID, 'vucV0300MonthList_LnkYotei')
+        log.debug('link for next month : tag={} href={} visible={}'.format(e_next_link.tag_name, e_next_link.get_attribute('href'), e_next_link.is_displayed()))
         e_next_link.click()
         helper.ss(name='future_bill')
 
@@ -109,15 +109,15 @@ try:
         fbill_file = '{}/請求予定の明細.txt'.format(outdir)
         log.info("writeting file: {}".format(fbill_file))
         try:
-            e_mesg = driver.find_element_by_id('DivErrorMessage')
+            e_mesg = driver.find_element(By.ID, 'DivErrorMessage')
             with open(fbill_file, 'wt') as fout:
                 fout.write('{}\n'.format(e_mesg.text.strip()))
             log.info('no future bill')
         except selenium.common.exceptions.NoSuchElementException:
-            e_bill = driver.find_element_by_id('LblSumUseValue')
+            e_bill = driver.find_element(By.ID, 'LblSumUseValue')
             log.debug('future bill : tag={} text={}'.format(e_bill.tag_name, e_bill.text))
-            e_table = driver.find_element_by_xpath('//table[@summary="ご利用年月日。"]')
-            e_trs = e_table.find_elements_by_xpath('//tr')
+            e_table = driver.find_element(By.XPATH, '//table[@summary="ご利用年月日。"]')
+            e_trs = e_table.find_elements(By.XPATH, '//tr')
             log.debug('trs: {}'.format(e_trs))
             with open(fbill_file, 'wt') as fout:
                 fout.write('{}\n'.format(e_bill.text.strip()))
